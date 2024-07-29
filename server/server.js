381533f4d06c5919a30f4be38541a996c90af71f
Guze <velardereynald@gmail.com>
2024-07-29 15:32:39 +0800

    update

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express()
+app.use(cors())
+
+const db = mysql/mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'lizaso_db'
+})
+app.get('/', (re, res) =>{
+    return res.json("From backend side")
+})
+
+app.get('/User_Account', (req, res) => {
+    const sql = "SELECT * FROM User_Account"
+    db.query(sql, (err, data) =>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("listening");
+})
