5ff32c9e6171fb53979467c12c54ae38be9778aa
Guze <velardereynald@gmail.com>
2024-08-10 23:32:48 +0800

    update

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,3 +1,180 @@
+// import {
+//   AppBar,
+//   Badge,
+//   Box,
+//   IconButton,
+//   Stack,
+//   Toolbar,
+//   Tooltip,
+//   Typography,
+//   Menu,
+//   MenuItem,
+//   Avatar,
+//   Divider,
+//   ListItemIcon,
+//   //   useTheme,
+// } from "@mui/material";
+
+// // icons
+// import PersonAdd from "@mui/icons-material/PersonAdd";
+// import Settings from "@mui/icons-material/Settings";
+// import Logout from "@mui/icons-material/Logout";
+
+// import React, { useState } from "react";
+// import { BsBell } from "react-icons/bs";
+// import { FiMenu, FiMoon, FiSun } from "react-icons/fi";
+// import UserImage from "../admin-components/UserImage";
+
+// const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
+//   const [anchorEl, setAnchorEl] = useState(null);
+//   const open = Boolean(anchorEl);
+//   const handleClick = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+//   //   const colorMode = useColorTheme();
+//   //   const theme = useTheme();
+
+//   //   const currentTheme = theme.palette.mode;
+
+//   return (
+//     <AppBar
+//       position="fixed"
+//       sx={{
+//         width: { md: `calc(100% - ${sideBarWidth}px)` },
+//         ml: { md: `${sideBarWidth}px` },
+//         boxShadow: "unset",
+//         backgroundColor: "background.paper",
+//         color: "text.primary",
+//         borderBottomWidth: 1,
+//         borderBottomColor: "divider",
+//       }}
+//     >
+//       <Toolbar>
+//         <Box
+//           sx={{
+//             display: "flex",
+//             justifyContent: "space-between",
+//             alignItems: "center",
+//             width: "100%",
+//           }}
+//         >
+//           <Box
+//             sx={{
+//               display: "flex",
+//               alignItems: "center",
+//               gap: 1,
+//             }}
+//           >
+//             <Tooltip title="Menu" arrow>
+//               <IconButton
+//                 aria-label="open drawer"
+//                 edge="start"
+//                 onClick={handleDrawerToggle}
+//                 sx={{ mr: 2, display: { md: "none" } }}
+//               >
+//                 <FiMenu />
+//               </IconButton>
+//             </Tooltip>
+
+//             <Typography
+//               variant="h5"
+//               sx={{ display: { xs: "none", sm: "block" } }}
+//             >
+//               Dashboard
+//             </Typography>
+//           </Box>
+//           <Stack direction="row" spacing={1} alignItems="center">
+//             <Tooltip title="Notifications" arrow>
+//               <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
+//                 <Badge color="error" variant="dot">
+//                   <BsBell />
+//                 </Badge>
+//               </IconButton>
+//             </Tooltip>
+//             <Tooltip title="Toggle Theme" arrow>
+//               <IconButton
+//                 // onClick={colorMode.toggleColorMode}
+//                 sx={{ fontSize: "20px", color: "text.primary" }}
+//               >
+//                 {/* {currentTheme === "light" ? <FiMoon /> : <FiSun />} */}
+//               </IconButton>
+//             </Tooltip>
+
+//             <div className="" onClick={handleClick}>
+//               <UserImage />
+//             </div>
+//             <Menu
+//               anchorEl={anchorEl}
+//               id="account-menu"
+//               open={open}
+//               onClose={handleClose}
+//               onClick={handleClose}
+//               PaperProps={{
+//                 elevation: 0,
+//                 sx: {
+//                   overflow: "visible",
+//                   filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+//                   mt: 1.5,
+//                   "& .MuiAvatar-root": {
+//                     width: 32,
+//                     height: 32,
+//                     ml: -0.5,
+//                     mr: 1,
+//                   },
+//                   "&::before": {
+//                     content: '""',
+//                     display: "block",
+//                     position: "absolute",
+//                     top: 0,
+//                     right: 14,
+//                     width: 10,
+//                     height: 10,
+//                     bgcolor: "background.paper",
+//                     transform: "translateY(-50%) rotate(45deg)",
+//                     zIndex: 0,
+//                   },
+//                 },
+//               }}
+//               transformOrigin={{ horizontal: "right", vertical: "top" }}
+//               anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+//             >
+//               <MenuItem onClick={handleClose}>
+//                 <Avatar /> Profile
+//               </MenuItem>
+//               <MenuItem onClick={handleClose}>
+//                 <Avatar /> My account
+//               </MenuItem>
+//               <Divider />
+//               <MenuItem onClick={handleClose}>
+//                 <ListItemIcon>
+//                   <PersonAdd fontSize="small" />
+//                 </ListItemIcon>
+//                 Add another account
+//               </MenuItem>
+//               <MenuItem onClick={handleClose}>
+//                 <ListItemIcon>
+//                   <Settings fontSize="small" />
+//                 </ListItemIcon>
+//                 Settings
+//               </MenuItem>
+//               <MenuItem onClick={handleClose}>
+//                 <ListItemIcon>
+//                   <Logout fontSize="small" />
+//                 </ListItemIcon>
+//                 Logout
+//               </MenuItem>
+//             </Menu>
+//           </Stack>
+//         </Box>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// };
+
+// export default Navbar;
 import {
   AppBar,
   Badge,
@@ -12,13 +189,14 @@ import {
   Avatar,
   Divider,
   ListItemIcon,
-  //   useTheme,
 } from "@mui/material";
 
 // icons
 import PersonAdd from "@mui/icons-material/PersonAdd";
 import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 
 import React, { useState } from "react";
 import { BsBell } from "react-icons/bs";
@@ -34,10 +212,9 @@ const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  //   const colorMode = useColorTheme();
-  //   const theme = useTheme();
 
-  //   const currentTheme = theme.palette.mode;
+  // Replace this with the actual user's name
+  const userName = "John Doe";
 
   return (
     <AppBar
@@ -86,26 +263,27 @@ const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
               Dashboard
             </Typography>
           </Box>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Tooltip title="Notifications" arrow>
+          <Stack direction="row" spacing={2} alignItems="center">
+            <Tooltip title="Messages" arrow>
               <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
                 <Badge color="error" variant="dot">
-                  <BsBell />
+                  <MailOutlineIcon />
                 </Badge>
               </IconButton>
             </Tooltip>
-            <Tooltip title="Toggle Theme" arrow>
-              <IconButton
-                // onClick={colorMode.toggleColorMode}
-                sx={{ fontSize: "20px", color: "text.primary" }}
-              >
-                {/* {currentTheme === "light" ? <FiMoon /> : <FiSun />} */}
+            <Tooltip title="Notifications" arrow>
+              <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
+                <Badge color="error" variant="dot">
+                  <NotificationsNoneOutlinedIcon />
+                </Badge>
               </IconButton>
             </Tooltip>
-
-            <div className="" onClick={handleClick}>
+            <div onClick={handleClick}>
               <UserImage />
             </div>
+            <Typography variant="body1" sx={{ fontWeight: "500" }}>
+              {userName}
+            </Typography>
             <Menu
               anchorEl={anchorEl}
               id="account-menu"
