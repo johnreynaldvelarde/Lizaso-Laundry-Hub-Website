72ceae0f8fba0ac5354253a38af9b36cc1710e86
Guze <velardereynald@gmail.com>
2024-08-12 00:45:12 +0800

    update 8/12/2024

--- client/src/components/admin-components/UserImage.jsx
+++ client/src/components/admin-components/UserImage.jsx
@@ -3,9 +3,11 @@ import profileImage from "../../assets/images/119657882.jfif";
 
 const UserImage = () => {
   return (
-    <span className="userImage flex w-[40px] h-[40px] overflow-hidden cursor-pointer">
-      <img src={profileImage} className="w-[100%] h-[100%] object-cover" />
-    </span>
+    <div className="border-2 border-[#5787C8] p-0.5 rounded-full">
+      <span className="userImage flex w-[40px] h-[40px] overflow-hidden">
+        <img src={profileImage} className="w-[100%] h-[100%] object-cover" />
+      </span>
+    </div>
   );
 };
 
--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,180 +1,3 @@
-// import {
-//   AppBar,
-//   Badge,
-//   Box,
-//   IconButton,
-//   Stack,
-//   Toolbar,
-//   Tooltip,
-//   Typography,
-//   Menu,
-//   MenuItem,
-//   Avatar,
-//   Divider,
-//   ListItemIcon,
-//   //   useTheme,
-// } from "@mui/material";
-
-// // icons
-// import PersonAdd from "@mui/icons-material/PersonAdd";
-// import Settings from "@mui/icons-material/Settings";
-// import Logout from "@mui/icons-material/Logout";
-
-// import React, { useState } from "react";
-// import { BsBell } from "react-icons/bs";
-// import { FiMenu, FiMoon, FiSun } from "react-icons/fi";
-// import UserImage from "../admin-components/UserImage";
-
-// const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
-//   const [anchorEl, setAnchorEl] = useState(null);
-//   const open = Boolean(anchorEl);
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
-//   //   const colorMode = useColorTheme();
-//   //   const theme = useTheme();
-
-//   //   const currentTheme = theme.palette.mode;
-
-//   return (
-//     <AppBar
-//       position="fixed"
-//       sx={{
-//         width: { md: `calc(100% - ${sideBarWidth}px)` },
-//         ml: { md: `${sideBarWidth}px` },
-//         boxShadow: "unset",
-//         backgroundColor: "background.paper",
-//         color: "text.primary",
-//         borderBottomWidth: 1,
-//         borderBottomColor: "divider",
-//       }}
-//     >
-//       <Toolbar>
-//         <Box
-//           sx={{
-//             display: "flex",
-//             justifyContent: "space-between",
-//             alignItems: "center",
-//             width: "100%",
-//           }}
-//         >
-//           <Box
-//             sx={{
-//               display: "flex",
-//               alignItems: "center",
-//               gap: 1,
-//             }}
-//           >
-//             <Tooltip title="Menu" arrow>
-//               <IconButton
-//                 aria-label="open drawer"
-//                 edge="start"
-//                 onClick={handleDrawerToggle}
-//                 sx={{ mr: 2, display: { md: "none" } }}
-//               >
-//                 <FiMenu />
-//               </IconButton>
-//             </Tooltip>
-
-//             <Typography
-//               variant="h5"
-//               sx={{ display: { xs: "none", sm: "block" } }}
-//             >
-//               Dashboard
-//             </Typography>
-//           </Box>
-//           <Stack direction="row" spacing={1} alignItems="center">
-//             <Tooltip title="Notifications" arrow>
-//               <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
-//                 <Badge color="error" variant="dot">
-//                   <BsBell />
-//                 </Badge>
-//               </IconButton>
-//             </Tooltip>
-//             <Tooltip title="Toggle Theme" arrow>
-//               <IconButton
-//                 // onClick={colorMode.toggleColorMode}
-//                 sx={{ fontSize: "20px", color: "text.primary" }}
-//               >
-//                 {/* {currentTheme === "light" ? <FiMoon /> : <FiSun />} */}
-//               </IconButton>
-//             </Tooltip>
-
-//             <div className="" onClick={handleClick}>
-//               <UserImage />
-//             </div>
-//             <Menu
-//               anchorEl={anchorEl}
-//               id="account-menu"
-//               open={open}
-//               onClose={handleClose}
-//               onClick={handleClose}
-//               PaperProps={{
-//                 elevation: 0,
-//                 sx: {
-//                   overflow: "visible",
-//                   filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-//                   mt: 1.5,
-//                   "& .MuiAvatar-root": {
-//                     width: 32,
-//                     height: 32,
-//                     ml: -0.5,
-//                     mr: 1,
-//                   },
-//                   "&::before": {
-//                     content: '""',
-//                     display: "block",
-//                     position: "absolute",
-//                     top: 0,
-//                     right: 14,
-//                     width: 10,
-//                     height: 10,
-//                     bgcolor: "background.paper",
-//                     transform: "translateY(-50%) rotate(45deg)",
-//                     zIndex: 0,
-//                   },
-//                 },
-//               }}
-//               transformOrigin={{ horizontal: "right", vertical: "top" }}
-//               anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-//             >
-//               <MenuItem onClick={handleClose}>
-//                 <Avatar /> Profile
-//               </MenuItem>
-//               <MenuItem onClick={handleClose}>
-//                 <Avatar /> My account
-//               </MenuItem>
-//               <Divider />
-//               <MenuItem onClick={handleClose}>
-//                 <ListItemIcon>
-//                   <PersonAdd fontSize="small" />
-//                 </ListItemIcon>
-//                 Add another account
-//               </MenuItem>
-//               <MenuItem onClick={handleClose}>
-//                 <ListItemIcon>
-//                   <Settings fontSize="small" />
-//                 </ListItemIcon>
-//                 Settings
-//               </MenuItem>
-//               <MenuItem onClick={handleClose}>
-//                 <ListItemIcon>
-//                   <Logout fontSize="small" />
-//                 </ListItemIcon>
-//                 Logout
-//               </MenuItem>
-//             </Menu>
-//           </Stack>
-//         </Box>
-//       </Toolbar>
-//     </AppBar>
-//   );
-// };
-
-// export default Navbar;
 import {
   AppBar,
   Badge,
@@ -195,26 +18,47 @@ import {
 import PersonAdd from "@mui/icons-material/PersonAdd";
 import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 
 import React, { useState } from "react";
-import { BsBell } from "react-icons/bs";
-import { FiMenu, FiMoon, FiSun } from "react-icons/fi";
+import { FiMenu } from "react-icons/fi";
 import UserImage from "../admin-components/UserImage";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaRegBell } from "react-icons/fa";
 
 const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [messageAnchorEl, setMessageAnchorEl] = useState(null);
+  const [notificationAnchorEl, setNotificationAnchorEl] = useState(null);
+
   const open = Boolean(anchorEl);
+  const openMessages = Boolean(messageAnchorEl);
+  const openNotifications = Boolean(notificationAnchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
 
-  // Replace this with the actual user's name
-  const userName = "John Doe";
+  const handleMessagesClick = (event) => {
+    setMessageAnchorEl(event.currentTarget);
+  };
+
+  const handleMessagesClose = () => {
+    setMessageAnchorEl(null);
+  };
+
+  const handleNotificationsClick = (event) => {
+    setNotificationAnchorEl(event.currentTarget);
+  };
+
+  const handleNotificationsClose = () => {
+    setNotificationAnchorEl(null);
+  };
+
+  const userName = "Rose Oriana";
 
   return (
     <AppBar
@@ -263,25 +107,82 @@ const Navbar = ({ sideBarWidth, handleDrawerToggle }) => {
               Dashboard
             </Typography>
           </Box>
-          <Stack direction="row" spacing={2} alignItems="center">
+
+          <Stack direction="row" spacing={1.5} alignItems="center">
             <Tooltip title="Messages" arrow>
-              <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
-                <Badge color="error" variant="dot">
-                  <MailOutlineIcon />
+              <IconButton
+                className="rounded-circle"
+                onClick={handleMessagesClick}
+              >
+                <Badge badgeContent={4} color="error">
+                  <MdOutlineMailOutline />
                 </Badge>
               </IconButton>
             </Tooltip>
+            <Menu
+              anchorEl={messageAnchorEl}
+              open={openMessages}
+              onClose={handleMessagesClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem onClick={handleMessagesClose}>Message 1</MenuItem>
+              <MenuItem onClick={handleMessagesClose}>Message 2</MenuItem>
+              <MenuItem onClick={handleMessagesClose}>Message 3</MenuItem>
+            </Menu>
+
             <Tooltip title="Notifications" arrow>
-              <IconButton sx={{ fontSize: "20px", color: "text.primary" }}>
-                <Badge color="error" variant="dot">
-                  <NotificationsNoneOutlinedIcon />
+              <IconButton
+                className="rounded-circle"
+                onClick={handleNotificationsClick}
+              >
+                <Badge badgeContent={10} color="error">
+                  <FaRegBell />
                 </Badge>
               </IconButton>
             </Tooltip>
-            <div onClick={handleClick}>
+            <Menu
+              anchorEl={notificationAnchorEl}
+              open={openNotifications}
+              onClose={handleNotificationsClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                },
+              }}
+              transformOrigin={{ horizontal: "right", vertical: "top" }}
+              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+              <MenuItem onClick={handleNotificationsClose}>
+                Notification 1
+              </MenuItem>
+              <MenuItem onClick={handleNotificationsClose}>
+                Notification 2
+              </MenuItem>
+              <MenuItem onClick={handleNotificationsClose}>
+                Notification 3
+              </MenuItem>
+            </Menu>
+
+            <div>
               <UserImage />
             </div>
-            <Typography variant="body1" sx={{ fontWeight: "500" }}>
+            <Typography
+              variant="body1"
+              sx={{ fontWeight: "500", cursor: "pointer" }}
+              onClick={handleClick}
+            >
               {userName}
             </Typography>
             <Menu
