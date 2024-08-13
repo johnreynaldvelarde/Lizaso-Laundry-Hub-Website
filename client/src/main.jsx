e85260fb237faede1e01f31c597c359d7d6eba6b
Guze <Guze@LENOVO-PC>
2024-08-13 08:58:47 +0800

    update l

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme.js";
-import "./index.css";
+import "../src/styles/index.css";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
