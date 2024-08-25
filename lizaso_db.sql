c7050bcbca04ef60bff66672cc4c31dd2b645957
Guze <velardereynald@gmail.com>
2024-08-25 18:15:56 +0800

    update now 2

--- lizaso_db.sql
+++ lizaso_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 23, 2024 at 10:27 AM
+-- Generation Time: Aug 25, 2024 at 12:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,16 +42,6 @@ CREATE TABLE `customers` (
   `date_created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`id`, `c_firstname`, `c_middlename`, `c_lastname`, `c_username`, `c_password`, `c_number`, `c_email`, `isAgreement`, `isOnline`, `isArchive`, `date_created`) VALUES
-(23, 'Rose', '', 'Oriana', 'roseoriana16', '$2b$12$6Miu43ZG6V3jRX03S15Sjeqc1cJ4JD8sb6SVHwzzO5B2Rn1gneob6', '', '', 1, 0, 0, '2024-08-19 09:06:50'),
-(24, '121', '', '212', '1212', '$2b$12$krY43.1hJ0xrOHC74K7Z9.4TzC/6kLxh2AJCua1KO/qd3hlP/P4Mm', '', '', 1, 0, 0, '2024-08-19 15:02:07'),
-(25, 'alexia', '', 'midgar', 'alexia16', '$2b$12$KEDtwuAymtkJOBEWBFUBPeRfCSWEY0USeRGb940.6AItWiTxqSYOG', '', '', 1, 0, 0, '2024-08-19 15:07:23'),
-(26, 'alexia', '', 'midgar', 'alexia12', '$2b$12$jbawVkGBAlnbVBzMtwgKzO2SsLU5zrlyuL41jGvjHbZxUw1coUOaa', '', '', 1, 0, 0, '2024-08-19 15:09:34');
-
 -- --------------------------------------------------------
 
 --
@@ -63,7 +53,8 @@ CREATE TABLE `laundry_unit` (
   `store_id` bigint(20) NOT NULL,
   `unit_name` varchar(100) NOT NULL,
   `date_created` datetime NOT NULL,
-  `isUnitStatus` tinyint(4) NOT NULL
+  `isUnitStatus` tinyint(4) NOT NULL,
+  `isArchive` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -74,7 +65,7 @@ CREATE TABLE `laundry_unit` (
 
 CREATE TABLE `stores` (
   `id` bigint(20) NOT NULL,
-  `store_id` varchar(50) NOT NULL,
+  `store_no` varchar(50) NOT NULL,
   `store_name` varchar(255) NOT NULL,
   `store_address` text NOT NULL,
   `store_contact` varchar(50) NOT NULL,
@@ -89,11 +80,8 @@ CREATE TABLE `stores` (
 -- Dumping data for table `stores`
 --
 
-INSERT INTO `stores` (`id`, `store_id`, `store_name`, `store_address`, `store_contact`, `is_main_store`, `updated_at`, `date_created`, `isStatus`, `isArchive`) VALUES
-(11, 'LIZASO-1723872684799', 'Main Store', 'Main Address', 'Main Contact', 1, '0000-00-00 00:00:00', '2024-08-17 13:31:24', 1, 0),
-(27, 'LIZASO3314', 'Lizaso Balagtas', 'Balagtas, Bulacan', '09182727061', 0, '2024-08-21 16:10:19', '2024-08-21 16:10:19', 0, 0),
-(28, 'LIZASO5791', 'Lizaso Malolos', 'Malolos, Bulacan', '09182745061', 0, '2024-08-21 16:25:35', '2024-08-21 16:25:35', 0, 0),
-(29, '1', '1', '1', '1', 0, '2024-08-21 16:36:13', '2024-08-21 16:36:13', 0, 0);
+INSERT INTO `stores` (`id`, `store_no`, `store_name`, `store_address`, `store_contact`, `is_main_store`, `updated_at`, `date_created`, `isStatus`, `isArchive`) VALUES
+(1, 'LIZASO-1724580830730', 'Main Store', 'Main Address', 'Main Contact', 1, '0000-00-00 00:00:00', '2024-08-25 18:13:50', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -122,7 +110,7 @@ CREATE TABLE `user_account` (
 --
 
 INSERT INTO `user_account` (`id`, `store_id`, `username`, `email`, `mobile_number`, `first_name`, `middle_name`, `last_name`, `isRole`, `isOnline`, `isStatus`, `isArchive`, `date_created`) VALUES
-(17, 11, 'admin', 'admin@example.com', '', 'Rose', '', 'Oriana', 0, 0, 1, 0, '2024-08-17 13:31:24');
+(1, 1, 'admin', 'admin@example.com', '', 'Admin', '', 'User', 0, 1, 1, 0, '2024-08-25 18:13:50');
 
 -- --------------------------------------------------------
 
@@ -150,7 +138,7 @@ CREATE TABLE `user_security` (
 --
 
 INSERT INTO `user_security` (`id`, `user_id`, `password`, `password_salt`, `mfa_enabled`, `mfa_secret`, `failed_login_attempts`, `account_locked`, `lockout_time`, `last_login`, `last_logout`, `last_password_change`) VALUES
-(11, 17, '$2b$10$uUNl5VC3dqVjhkZeJdY49ejCzI8ApM3TxYZq1Kw.btsoB8our24HG', '$2b$10$8T8j0soOf3267M43ObQvc.', 0, '', 0, 0, '2024-08-17 05:31:24', NULL, NULL, NULL);
+(1, 1, '$2b$10$ZuHv8Lnrfhw8pJl.ucgC7u8eoJdkQweuXcC2TYOi.tjjG.g.wvWNC', '$2b$10$XC2dRNqztIbvhJKIdc1dde', 0, '', 0, 0, '2024-08-25 10:13:50', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -197,7 +185,7 @@ ALTER TABLE `user_security`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `laundry_unit`
@@ -209,19 +197,19 @@ ALTER TABLE `laundry_unit`
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user_account`
 --
 ALTER TABLE `user_account`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user_security`
 --
 ALTER TABLE `user_security`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
