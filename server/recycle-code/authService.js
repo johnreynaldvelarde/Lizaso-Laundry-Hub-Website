66fbdf57a8bc9be889085328cde6d041d26f6cde
Admin <Admin@DESKTOP-STAD1GF>
2024-10-22 22:32:31 +0800

    update

--- server/recycle-code/authService.js
+++ server/recycle-code/authService.js
@@ -221,6 +221,100 @@ export const handleRegister = async (req, res, db) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
+// export const handleRegister = async (req, res, db) => {
+//   const {
+//     c_firstname,
+//     c_middlename,
+//     c_lastname,
+//     c_username,
+//     c_password,
+//     isAgreement,
+//     c_email,
+//     c_number,
+//   } = req.body;
+
+//   const newCustomerId = generateBigIntCustomerId();
+
+//   try {
+//     // Hash the customer's password
+//     const hashedPassword = await hashPassword(c_password);
+
+//     // Generate password salt (if needed)
+//     const passwordSalt = await generatePasswordSalt();
+
+//     // Begin transaction
+//     await db.beginTransaction();
+
+//     // Insert customer into Customers table
+//     // const [customerResult] = await db.query(
+//     //   `INSERT INTO Customer (id, c_firstname, c_middlename, c_lastname, c_username, c_number, c_email, isAgreement, date_created)
+//     //    VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())`,
+//     //   [
+//     //     newCustomerId,
+//     //     c_firstname,
+//     //     c_middlename,
+//     //     c_lastname,
+//     //     c_username,
+//     //     c_number,
+//     //     c_email,
+//     //     isAgreement,
+//     //   ]
+//     // );
+
+//     const [customerResult] = await db.query(
+//       `INSERT INTO Customer (id, c_firstname, c_middlename, c_lastname, c_username, c_number, c_email, isAgreement, date_created)
+//        VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())`,
+//       [
+//         newCustomerId,
+//         c_firstname,
+//         c_middlename,
+//         c_lastname,
+//         c_username,
+//         c_number,
+//         c_email,
+//         isAgreement,
+//       ]
+//     );
+
+//     // Check if the insertion was successful
+//     if (customerResult.affectedRows === 0) {
+//       throw new Error("Failed to insert customer");
+//     }
+
+//     // const customerId = customerResult.insertId;
+
+//     // Insert customer security data into Customer_Security table
+//     await db.query(
+//       `INSERT INTO Customer_Security (customer_id, c_password, c_password_salt, mfa_enabled, failed_login_attempts, account_locked, last_login, last_logout, last_password_change)
+//        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+//       [
+//         newCustomerId,
+//         hashedPassword,
+//         passwordSalt,
+//         false,
+//         0,
+//         false,
+//         null,
+//         null,
+//         null,
+//       ]
+//     );
+
+//     // Commit transaction
+//     await db.commit();
+
+//     // Respond with success
+//     res
+//       .status(201)
+//       .json({ success: true, message: "Customer registered successfully" });
+//   } catch (error) {
+//     // Rollback transaction in case of error
+//     await db.rollback();
+
+//     console.error("Error registering customer:", error);
+//     res.status(500).json({ success: false, message: "Server error" });
+//   }
+// };
 
 export const handleRefreshToken = async (req, res) => {
   const { refreshToken } = req.cookies;
@@ -271,6 +365,8 @@ export const getUserDetails = async (req, res, db) => {
   if (!decoded) {
     return res.status(401).json({ success: false, message: "Invalid token" });
   }
+
+  // console.log("Decoded Token1:", decoded);
 
   const { userId, username } = decoded;
 
@@ -344,144 +440,210 @@ export const getUserDetails = async (req, res, db) => {
   }
 };
 
-export const handleLoginMobile = async (req, res, db) => {
-  const { username, password } = req.body;
-  try {
-    let user, userType, roleName, permissions;
+// export const getUserDetails = async (req, res, db) => {
+//   const authHeader = req.headers.authorization;
 
-    // Check User_Account table first
-    const [userAccountResults] = await db.query(
-      `SELECT ua.*, rp.role_name, 
-              rp.can_read, rp.can_write, rp.can_edit, rp.can_delete 
-       FROM User_Account ua
-       JOIN Roles_Permissions rp ON ua.role_permissions_id = rp.id
-       WHERE ua.username = ?`,
-      [username]
-    );
+//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//     return res.status(401).json({ success: false, message: "Unauthorized" });
+//   }
 
-    if (userAccountResults.length > 0) {
-      user = userAccountResults[0];
-      const [secResults] = await db.query(
-        "SELECT * FROM User_Security WHERE user_id = ?",
-        [user.id]
-      );
+//   const token = authHeader.split(" ")[1];
+//   const decoded = decodeToken(token);
 
-      if (secResults.length === 0) {
-        return res.status(200).json({
-          success: false,
-          message: "User security details not found.",
-        });
-      }
+//   if (!decoded) {
+//     return res.status(401).json({ success: false, message: "Invalid token" });
+//   }
 
-      const userSecurity = secResults[0];
-      const passwordMatch = await comparePassword(
-        password,
-        userSecurity.password
-      );
+//   console.log("Decoded Token1:", decoded);
 
-      if (!passwordMatch) {
-        return res
-          .status(200)
-          .json({ success: false, message: "Invalid username or password." });
-      }
+//   const userId = decoded.userId;
+//   const username = decoded.username;
 
-      // Get role name and permissions
-      roleName = user.role_name;
-      permissions = {
-        can_read: user.can_read,
-        can_write: user.can_write,
-        can_edit: user.can_edit,
-        can_delete: user.can_delete,
-      };
+//   try {
+//     // Check if the user exists in the User_Account table
+//     const [userAccountResults] = await db.query(
+//       "SELECT * FROM User_Account WHERE id = ? AND username = ?",
+//       [userId, username]
+//     );
 
-      // Update isOnline status to 1
-      await db.query("UPDATE User_Account SET isOnline = 1 WHERE id = ?", [
-        user.id,
-      ]);
+//     if (userAccountResults.length > 0) {
+//       const user = userAccountResults[0];
+//       return res.status(200).json({
+//         success: true,
+//         user: {
+//           userId: user.id,
+//           storeId: user.store_id,
+//           firstname: user.first_name,
+//           middlename: user.middle_name,
+//           lastname: user.last_name,
+//           email: user.email,
+//           phone: user.mobile_number,
+//           fullName: `${user.first_name} ${user.last_name}`,
+//           username: user.username,
+//           userType: user.isRole,
+//         },
+//       });
+//     } else {
+//       // Check if the customer exists in the Customer table
+//       const [customerResults] = await db.query(
+//         "SELECT * FROM Customer WHERE id = ?",
+//         [userId]
+//       );
 
-      const actionType = ActionTypes.AUTHENTICATION;
-      const actionDescription = ActionDescriptions[actionType].LOGIN(username);
+//       if (customerResults.length > 0) {
+//         const customer = customerResults[0];
+//         return res.status(200).json({
+//           success: true,
+//           user: {
+//             userId: customer.id,
+//             storeId: customer.store_id,
+//             fullName: `${customer.c_firstname} ${customer.c_lastname}`,
+//             username: customer.c_username,
+//           },
+//         });
+//       } else {
+//         return res
+//           .status(404)
+//           .json({ success: false, message: "User or Customer not found" });
+//       }
+//     }
+//   } catch (error) {
+//     console.error("Error fetching user details:", error);
+//     return res.status(500).json({ success: false, message: "Server error" });
+//   }
+// };
 
-      await logActivity(db, user.id, roleName, actionType, actionDescription);
-    } else {
-      // If not in User_Account, check Customers table
-      const [customerAccountResults] = await db.query(
-        "SELECT * FROM Customer WHERE c_username = ?",
-        [username]
-      );
+// export const handleLogin = async (req, res, db) => {
+//   const { username, password } = req.body;
 
-      if (customerAccountResults.length === 0) {
-        return res
-          .status(404)
-          .json({ success: false, message: "User not found." });
-      }
+//   try {
+//     let user, userType;
 
-      user = customerAccountResults[0];
+//     // Check User_Account table first
+//     const [userAccountResults] = await db.query(
+//       "SELECT * FROM User_Account WHERE username = ?",
+//       [username]
+//     );
 
-      const [secCustomerResults] = await db.query(
-        "SELECT * FROM Customer_Security WHERE customer_id = ?",
-        [user.id]
-      );
+//     if (userAccountResults.length > 0) {
+//       user = userAccountResults[0];
+//       const [secResults] = await db.query(
+//         "SELECT * FROM User_Security WHERE user_id = ?",
+//         [user.id]
+//       );
 
-      if (secCustomerResults.length === 0) {
-        return res.status(404).json({
-          success: false,
-          message: "Customer security details not found.",
-        });
-      }
+//       if (secResults.length === 0) {
+//         return res.status(404).json({
+//           success: false,
+//           message: "User security details not found.",
+//         });
+//       }
 
-      const customerSecurity = secCustomerResults[0];
-      const passwordMatch = await comparePassword(
-        password,
-        customerSecurity.c_password
-      );
+//       const userSecurity = secResults[0];
+//       const passwordMatch = await comparePassword(
+//         password,
+//         userSecurity.password
+//       );
 
-      if (!passwordMatch) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Invalid username or password." });
-      }
+//       if (!passwordMatch) {
+//         return res
+//           .status(401)
+//           .json({ success: false, message: "Invalid username or password." });
+//       }
 
-      // Set user type for customers
-      userType = "Customer";
+//       userType =
+//         user.isRole === 0
+//           ? "Admin"
+//           : user.isRole === 1
+//           ? "Manager"
+//           : user.isRole === 2
+//           ? "Staff"
+//           : "Delivery Staff";
 
-      // Update isOnline status to 1
-      await db.query("UPDATE Customer SET isOnline = 1 WHERE id = ?", [
-        user.id,
-      ]);
-    }
+//       // Update isOnline status to 1
+//       await db.query("UPDATE User_Account SET isOnline = 1 WHERE id = ?", [
+//         user.id,
+//       ]);
 
-    // Generate JWT tokens
-    const accessToken = createToken(
-      { userId: user.id, username, userType, roleName, permissions },
-      process.env.ACCESS_TOKEN_SECRET,
-      process.env.JWT_EXPIRES_IN
-    );
-    const refreshToken = createToken(
-      { userId: user.id, username, userType, roleName, permissions },
-      process.env.REFRESH_TOKEN_SECRET,
-      process.env.JWT_REFRESH_EXPIRES_IN
-    );
+//       const actionType = ActionTypes.AUTHENTICATION;
+//       const actionDescription = ActionDescriptions[actionType].LOGIN(username);
 
-    // Set the refresh token in an HTTP-only cookie
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
-      sameSite: "Strict", // Adjust as needed
-    });
+//       await logActivity(db, user.id, userType, actionType, actionDescription);
+//     } else {
+//       // If not in User_Account, check Customers table
+//       const [customerAccountResults] = await db.query(
+//         "SELECT * FROM Customer WHERE c_username = ?",
+//         [username]
+//       );
 
-    // Respond with access token, user type, role name, and permissions
-    return res.status(200).json({
-      success: true,
-      userType,
-      roleName,
-      permissions,
-      accessToken,
-    });
-  } catch (err) {
-    console.error("Error handling login:", err);
-    return res
-      .status(500)
-      .json({ success: false, message: "Internal Server Error" });
-  }
-};
+//       if (customerAccountResults.length === 0) {
+//         return res
+//           .status(404)
+//           .json({ success: false, message: "User not found." });
+//       }
+
+//       user = customerAccountResults[0];
+//       const [secCustomerResults] = await db.query(
+//         "SELECT * FROM Customer_Security WHERE customer_id = ?",
+//         [user.id]
+//       );
+
+//       if (secCustomerResults.length === 0) {
+//         return res.status(404).json({
+//           success: false,
+//           message: "Customer security details not found.",
+//         });
+//       }
+
+//       const customerSecuirty = secCustomerResults[0];
+//       const passwordMatch = await comparePassword(
+//         password,
+//         customerSecuirty.c_password
+//       );
+
+//       if (!passwordMatch) {
+//         return res
+//           .status(401)
+//           .json({ success: false, message: "Invalid username or password." });
+//       }
+
+//       userType = "Customer";
+
+//       // Update isOnline status to 1
+//       await db.query("UPDATE Customer SET isOnline = 1 WHERE id = ?", [
+//         user.id,
+//       ]);
+//     }
+
+//     // Generate JWT tokens
+//     const accessToken = createToken(
+//       { userId: user.id, username, userType },
+//       process.env.ACCESS_TOKEN_SECRET,
+//       process.env.JWT_EXPIRES_IN
+//     );
+//     const refreshToken = createToken(
+//       { userId: user.id, username, userType },
+//       process.env.REFRESH_TOKEN_SECRET,
+//       process.env.JWT_REFRESH_EXPIRES_IN
+//     );
+
+//     // Set the refresh token in an HTTP-only cookie
+//     res.cookie("refreshToken", refreshToken, {
+//       httpOnly: true,
+//       secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
+//       sameSite: "Strict", // Adjust as needed
+//     });
+
+//     // Respond with access token and user type
+//     return res.status(200).json({
+//       success: true,
+//       userType,
+//       accessToken,
+//     });
+//   } catch (err) {
+//     console.error("Error handling login:", err);
+//     return res
+//       .status(500)
+//       .json({ success: false, message: "Internal Server Error" });
+//   }
+// };
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,7 +1,12 @@
-import express from 'express';
-import { getPool } from '../db/dbConfig.js';
-import { handleRegister, handleLogin,  getUserDetails  } from '../services/authService.js';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import { getPool } from "../db/dbConfig.js";
+import {
+  handleRegister,
+  handleLogin,
+  getUserDetails,
+  handleLoginMobile,
+} from "../services/authService.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -12,59 +17,83 @@ const withDatabaseConnection = (handler) => async (req, res) => {
   try {
     await handler(req, res, connection);
   } catch (error) {
-    console.error('Database operation error:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error("Database operation error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   } finally {
     connection.release();
   }
 };
 
 // Login route
-router.post('/login', withDatabaseConnection(async (req, res, connection) => {
-  await handleLogin(req, res, connection);
-}));
+router.post(
+  "/login",
+  withDatabaseConnection(async (req, res, connection) => {
+    try {
+      await handleLogin(req, res, connection);
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  })
+);
+
+router.post(
+  "/login-mobile",
+  withDatabaseConnection(async (req, res, connection) => {
+    try {
+      await handleLoginMobile(req, res, connection);
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  })
+);
 
 // Register route
-router.post('/register', withDatabaseConnection(async (req, res, connection) => {
-  await handleRegister(req, res, connection);
-}));
+router.post(
+  "/register",
+  withDatabaseConnection(async (req, res, connection) => {
+    await handleRegister(req, res, connection);
+  })
+);
 
 // Get user details route
-router.get('/user/me', withDatabaseConnection(async (req, res, connection) => {
-  await getUserDetails(req, res, connection);
-}));
+router.get(
+  "/user/me",
+  withDatabaseConnection(async (req, res, connection) => {
+    await getUserDetails(req, res, connection);
+  })
+);
 
 // Refresh token route
-router.get('/refresh-token', (req, res) => {
+router.get("/refresh-token", (req, res) => {
   handleRefreshToken(req, res);
 });
 
 // for refresh token
 function handleRefreshToken(req, res) {
-  const refreshToken = req.cookies.refreshToken; 
+  const refreshToken = req.cookies.refreshToken;
 
   if (!refreshToken) {
-    console.log('No refresh token provided');
+    console.log("No refresh token provided");
     return res.json({ success: false, message: "No refresh token provided" });
   }
 
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) {
-      console.log('Invalid refresh token:', err.message);
-      return res.status(403).json({ success: false, message: "Invalid refresh token" });
+      console.log("Invalid refresh token:", err.message);
+      return res
+        .status(403)
+        .json({ success: false, message: "Invalid refresh token" });
     }
 
     const accessToken = jwt.sign(
-      {userId: user.userId, username: user.username, storeId: user.store_id, },
+      { userId: user.userId, username: user.username, storeId: user.store_id },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '15m' } // Adjust expiration as needed
+      { expiresIn: "15m" } // Adjust expiration as needed
     );
 
     res.json({ success: true, accessToken });
   });
 }
-
-
 
 // Refresh token route
 // router.post('/refresh-token', async (req, res) => {
@@ -81,8 +110,6 @@ function handleRefreshToken(req, res) {
 //   }
 // });
 
-
-
 const tokenBlacklist = new Set();
 
 // Middleware to check if a token is blacklisted
@@ -91,8 +118,8 @@ const isTokenBlacklisted = (token) => {
 };
 
 // Logout route
-router.post('/logout', (req, res) => {
-  console.log("Logout na")
+router.post("/logout", (req, res) => {
+  console.log("Logout na");
   const refreshToken = req.cookies.refreshToken;
 
   // Handle JWT invalidation
@@ -101,10 +128,10 @@ router.post('/logout', (req, res) => {
     tokenBlacklist.add(refreshToken);
 
     // Clear the refresh token cookie
-    res.clearCookie('refreshToken');
+    res.clearCookie("refreshToken");
   }
 
-  res.status(200).json({ success: true, message: 'Logged out successfully' });
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 });
 
 export default router;
