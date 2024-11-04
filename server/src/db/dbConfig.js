36fdb02dfde2fd40ac40e2636777e62d406c889b
Guze <velardereynald@gmail.com>
2024-11-04 21:05:09 +0800

    update

--- server/src/db/dbConfig.js
+++ server/src/db/dbConfig.js
@@ -1,21 +1,41 @@
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config({ path: "./.env" });
+
+// let pool;
+
+// const getPool = async () => {
+//   if (!pool) {
+//     pool = mysql.createPool({
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_DATABASE,
+//       waitForConnections: true,
+//       connectionLimit: 10, // Adjust based on your needs
+//       queueLimit: 0,
+//     });
+//   }
+//   return pool;
+// };
+
+// export { getPool };
+
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
+// Load environment variables from .env file
 dotenv.config({ path: "./.env" });
 
 let pool;
 
+// Construct the database URL from environment variables
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}`;
+
 const getPool = async () => {
   if (!pool) {
-    pool = mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      waitForConnections: true,
-      connectionLimit: 10, // Adjust based on your needs
-      queueLimit: 0,
-    });
+    pool = mysql.createPool(urlDB);
   }
   return pool;
 };
