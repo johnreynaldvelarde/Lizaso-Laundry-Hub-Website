72463e660eed84f5f58c46e189ce9107bdf5b45d
Guze <velardereynald@gmail.com>
2024-11-04 20:41:46 +0800

    update

--- server/src/db/dbConfig.js
+++ server/src/db/dbConfig.js
@@ -1,55 +1,7 @@
-// const mysql = require('mysql');
-// const dotnev = require('dotenv');
-// dotnev.config({path: './.env'})
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-
-// const connectToDatabase = () => {
-//   const db = mysql.createConnection({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE
-//   });
-
-//   db.connect((err) => {
-//     if (err) {
-//       console.error('Error connecting to the database:', err);
-//       process.exit(1);
-//     }
-//     console.log('Connected to the database.');
-//   });
-
-//   return db;
-// };
-
-// module.exports = { connectToDatabase };
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-
-// dotenv.config({ path: './.env' });
-
-// const connectToDatabase = async () => {
-//   try {
-//     const connection = await mysql.createConnection({
-//       host: process.env.DATABASE_HOST,
-//       user: process.env.DATABASE_USER,
-//       password: process.env.DATABASE_PASSWORD,
-//       database: process.env.DATABASE
-//     });
-//     console.log('Connected to the database.');
-//     return connection;
-//   } catch (err) {
-//     console.error('Error connecting to the database:', err);
-//     process.exit(1);
-//   }
-// };
-
-// export { connectToDatabase };
-
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 let pool;
 
@@ -62,13 +14,10 @@ const getPool = async () => {
       database: process.env.DATABASE,
       waitForConnections: true,
       connectionLimit: 10, // Adjust based on your needs
-      queueLimit: 0
+      queueLimit: 0,
     });
   }
   return pool;
 };
 
 export { getPool };
-
-
-
