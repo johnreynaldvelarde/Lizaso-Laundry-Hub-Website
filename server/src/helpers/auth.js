8aaf88b1657b2a5d3b58683dec4e6ce055769f0b
Guze <velardereynald@gmail.com>
2024-11-04 19:56:01 +0800

    update

--- server/src/helpers/auth.js
+++ server/src/helpers/auth.js
@@ -1,26 +1,26 @@
-import bcrypt, { hash } from 'bcrypt';
+import bcrypt from "bcryptjs";
 
-export const hashPassword = (password) =>{
-    return new Promise((resolve, reject) =>{
-        bcrypt.genSalt(12, (err, salt) =>{
-            if(err){
-                reject(err)
-            }
-            bcrypt.hash(password, salt, (err, hash) => {
-                if(err){
-                    reject(err)
-                }
-                resolve(hash)
-            })
-        })
-    })
-}
+export const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
 
 // Function to generate a password salt
 export const generatePasswordSalt = () => {
-    return bcrypt.genSalt(12);
+  return bcrypt.genSalt(12);
 };
 
-export const comparePassword = (password, hashed) =>{
-    return bcrypt.compare(password, hashed)
-}
+export const comparePassword = (password, hashed) => {
+  return bcrypt.compare(password, hashed);
+};
