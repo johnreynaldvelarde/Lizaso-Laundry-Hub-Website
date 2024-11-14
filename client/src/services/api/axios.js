d169a0a65c931d4552154a645b192f7175bbd01d
Guze <velardereynald@gmail.com>
2024-11-15 07:11:48 +0800

    1

--- client/src/services/api/axios.js
+++ client/src/services/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "";
 
 export const axiosPublic = axios.create({
   baseURL: API_URL,
