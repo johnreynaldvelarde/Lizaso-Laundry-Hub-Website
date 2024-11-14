f90d2674ad060acebe10f5d8c427dda7b4480b5a
Guze <velardereynald@gmail.com>
2024-11-15 06:49:51 +0800

    1

--- server/src/services/default.js
+++ server/src/services/default.js
@@ -63,6 +63,8 @@ export const ensureMainStoreExists = async (db) => {
         [storeId]
       );
 
+      console.log("STORE ID" + storeId);
+
       console.log(existingServiceTypes);
 
       // if (existingServiceTypes[0].count === 0) {
