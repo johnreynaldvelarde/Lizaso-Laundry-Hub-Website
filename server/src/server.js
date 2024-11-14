14b1a6d7b4d48eaaaf8aad25be1c8a24615939a3
Guze <velardereynald@gmail.com>
2024-11-15 07:21:38 +0800

    1

--- server/src/server.js
+++ server/src/server.js
@@ -26,10 +26,16 @@ import { setupSocketIO } from "./socket/socket.js";
 const app = express();
 const port = process.env.PORT || 3002;
 
+// const allowedOrigins = [
+//   process.env.NODE_ENV === "production"
+//     ? process.env.PRODUCTION_FRONTEND_URL
+//     : process.env.DEVELOPMENT_FRONTEND_URL,
+// ];
+
 const allowedOrigins = [
   process.env.NODE_ENV === "production"
     ? process.env.PRODUCTION_FRONTEND_URL
-    : process.env.DEVELOPMENT_FRONTEND_URL,
+    : "http://localhost:5173",
 ];
 
 app.use(express.json());
